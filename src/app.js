03a537bdcc7368238d6dd6ecddfc0384805db7b0
Koduri Sai Ganesh <kodurisaiganesh@gmail.com>
2026-08-10 23:48:30 +0530

    routing and request

--- src/app.js
+++ src/app.js
@@ -2,14 +2,31 @@ const express=require('express');
 const app=express();
 const port =3000;
 
-app.get('/',(req,res)=>{
+//https methods
+
+app.get('/user',(req,res)=>{
+    res.send({firstName:"SaiGanesh"
+    })
+})
+
+app.post('/users',(req,res)=>{
+    res.send("Sending the data Sccessfully")
+})
+// it will work for all https methods
+app.use('/',(req,res)=>{
     res.send('Hello world');
 })
+// it will print hello world because of order matters
 app.get('/hello',(req,res)=>{
     res.send("live server")
 })
+// if we use get  it will print the live server
 app.get('/hello1',(req,res)=>{
     res.send("live server")
+})
+
+app.get('/abcd',(req,res)=>{
+    res.send("Hiii");
 })
 
 
