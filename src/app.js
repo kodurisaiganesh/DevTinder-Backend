a70ad138325419e5194eea234cbcd51c785933a5
Koduri Sai Ganesh <kodurisaiganesh@gmail.com>
2026-08-11 23:04:54 +0530

    handling multiple routes

--- src/app.js
+++ src/app.js
@@ -2,34 +2,20 @@ const express=require('express');
 const app=express();
 const port =3000;
 
-//https methods
+app.use('/user',(req,res,next)=>{
+   // res.send("FirstLine");
+   console.log("First Line");
+   res.send("First Line")
+    next();
+},
+(req,res)=>{
+    res.send("First Line executed by using next fucntion")
 
-app.get('/user',(req,res)=>{
-    res.send({firstName:"SaiGanesh"
-    })
 })
-
-app.post('/users',(req,res)=>{
-    res.send("Sending the data Sccessfully")
+app.use('/users',(req,res)=>{
+    res.send("LastLine");
 })
-// it will work for all https methods
-app.use('/',(req,res)=>{
-    res.send('Hello world');
-})
-// it will print hello world because of order matters
-app.get('/hello',(req,res)=>{
-    res.send("live server")
-})
-// if we use get  it will print the live server
-app.get('/hello1',(req,res)=>{
-    res.send("live server")
-})
-
-app.get('/abcd',(req,res)=>{
-    res.send("Hiii");
-})
-
 
 app.listen(port,()=>{
-    console.log(`Listening the portt ${port}`)
+    console.log("Port is running");
 })
